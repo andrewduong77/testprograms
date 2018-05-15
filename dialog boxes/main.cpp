cca8a015e9727fb9a1db55209614516afffd38fc
Andrew Duong <andrewduong77@yahoo.com>
2018-05-15 12:17:24 -0500

    added dialog boxes

---
+++ dialog boxes/main.cpp
@@ -0,0 +1,11 @@
+#include <gtkmm.h>
+#include "mywindow.h"
+
+int main(int argc, char *argv[])
+{
+    Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv, "com.gtkmm.tutorial3.base");
+
+    mywindow window;
+
+    return app->run(window);
+}
